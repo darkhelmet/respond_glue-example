77f4cfa9ba4e69f3a90015d9584a638d7334bc51
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-08-20 19:55:39 -0600

    next step

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,50 +2,62 @@ class WidgetsController < ApplicationController
   def index
     @widgets = widget_class.all
     respond_to do |format|
+      format.html
       format.js { render(:json => @widgets) }
     end
   end
 
   def show
     @widget = widget_class.find(params[:id])
-    emit
+    respond_to do |format|
+      format.html
+      format.js { render(:json => @widget.to_json(:methods => [:valid])) }
+    end
   end
 
   def new
     @widget = widget_class.new
-    emit
+    respond_to do |format|
+      format.html
+    end
   end
 
   def edit
     @widget = widget_class.find(params[:id])
-    emit
+    respond_to do |format|
+      format.html
+      format.js { render(:json => @widget.to_json(:methods => [:valid])) }
+    end
   end
 
   def create
     @widget = widget_class.new(params[:widget])
     @widget.save
-    emit
+    respond_to do |format|
+      format.html
+      format.js { render(:json => @widget.to_json(:methods => [:valid])) }
+    end
   end
 
   def update
     @widget = widget_class.find(params[:id])
     @widget.update_attributes(params[:widget])
-    emit
+    respond_to do |format|
+      format.html
+      format.js { render(:json => @widget.to_json(:methods => [:valid])) }
+    end
   end
 
   def destroy
     @widget = widget_class.find(params[:id])
     @widget.destroy
-    emit
+    respond_to do |format|
+      format.html
+      format.js { head(:ok) }
+    end
   end
 
 protected
-
-  def emit
-    respond_to do |format|
-      format.js { render(:json => @widget.to_json(:methods => [:valid]))}
-    end
-  end
 
   def widget_class
     params[:controller].singularize.camelize.constantize
