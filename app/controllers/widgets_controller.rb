9da9d47ad0b79aea7e1b06ed4f5aed5b63f79bd4
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-08-20 20:23:06 -0600

    view changes so html works

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,7 +2,7 @@ class WidgetsController < ApplicationController
   def index
     @widgets = widget_class.all
     respond_to do |format|
-      format.html
+      format.html { render(:template => "widgets/index") }
       format.js { render(:json => @widgets) }
     end
   end
@@ -10,7 +10,7 @@ class WidgetsController < ApplicationController
   def show
     @widget = widget_class.find(params[:id])
     respond_to do |format|
-      format.html
+      format.html { render(:template => "widgets/show") }
       format.js { render(:json => @widget.to_json(:methods => [:valid])) }
     end
   end
@@ -18,32 +18,32 @@ class WidgetsController < ApplicationController
   def new
     @widget = widget_class.new
     respond_to do |format|
-      format.html
+      format.html { render(:template => "widgets/new") }
     end
   end
 
   def edit
     @widget = widget_class.find(params[:id])
     respond_to do |format|
-      format.html
+      format.html { render(:template => "widgets/edit") }
       format.js { render(:json => @widget.to_json(:methods => [:valid])) }
     end
   end
 
   def create
-    @widget = widget_class.new(params[:widget])
+    @widget = widget_class.new(params[widget_symbol])
     @widget.save
     respond_to do |format|
-      format.html
+      format.html { redirect_to(@widget) }
       format.js { render(:json => @widget.to_json(:methods => [:valid])) }
     end
   end
 
   def update
     @widget = widget_class.find(params[:id])
-    @widget.update_attributes(params[:widget])
+    @widget.update_attributes(params[widget_symbol])
     respond_to do |format|
-      format.html
+      format.html { redirect_to(@widget) }
       format.js { render(:json => @widget.to_json(:methods => [:valid])) }
     end
   end
@@ -52,7 +52,7 @@ class WidgetsController < ApplicationController
     @widget = widget_class.find(params[:id])
     @widget.destroy
     respond_to do |format|
-      format.html
+      format.html { redirect_to(fancy_widgets_path) }
       format.js { head(:ok) }
     end
   end
@@ -64,6 +64,6 @@ protected
   end
 
   def widget_symbol
-    params[:controller].singular.intern
+    params[:controller].singularize.intern
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :widgets
+
+  map.resources :foos
+
   map.resources :fancy_widgets
   map.resources :weird_widgets
 
